52c074a3d23b0e03a677f5e65c6685c24e71bf67
tomfullylove <33106870+tomfullylove@users.noreply.github.com>
2017-11-07 13:28:53 +0000

    .

--- main.php
+++ main.php
@@ -22,7 +22,7 @@
                     <li><a href="#" class="top-sub">All</a></li>
                     <li><a href="#">Open</a></li>
                     <li><h3>More</h3></li>
-                    <li><a href="analytics.php" class="top-sub">Stats</a></li>
+                    <li><a href="analytics.php" class="top-sub">Analytics</a></li>
                     <li><a href="index.php">Log out</a></li>
                     
                 </ul>
