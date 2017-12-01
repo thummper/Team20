9535b3f42fdf412e1709f062736603fefc898440
tomfullylove <33106870+tomfullylove@users.noreply.github.com>
2017-12-01 21:32:55 +0000

    login (html + css)

--- helpdesk/index.php
+++ helpdesk/index.php
@@ -7,7 +7,11 @@
         <link rel="stylesheet" href="css/style.css" />
         
 	</head>
-	<body>
-        
+	<body class="login-body">
+        <form action="" class="login-form">
+            <input type="text" placeholder="Staff ID" name="stfID" required>
+            <input type="password" placeholder="Password" name="psw" class="login-field" required>
+            <button type="submit" class="login-button">Login</button>
+        </form>    
 	</body>
 </html>
