0b69e89e326c5265925e2ce3bc4189748de3a276
tomfullylove <33106870+tomfullylove@users.noreply.github.com>
2017-12-20 18:18:40 +0000

    css for login + menu

--- helpdesk/main.php
+++ helpdesk/main.php
@@ -8,6 +8,32 @@
         
 	</head>
 	<body>
-        
+ 
+     	<div class="sidebar">
+        	<div class="sidebar-top">
+                <h2>Operator</h2>
+                <p>Alice</p>
+            </div>
+            <div class="sidebar-mid">
+                <ul class="nav">
+                    <li><h3>Issues</h3></li>
+                    <li><a href="#" class="top-sub active">All</a></li>
+                    <li><a href="#">Open</a></li>
+                    <li><a href="#">Closed</a></li>
+                    <li><h3>Queries</h3></li>
+                    <li><a href="#" class="top-sub">All</a></li>
+                    <li><a href="#">Open</a></li>
+                    <li><h3>More</h3></li>
+                    <li><a href="#" class="top-sub">Analytics</a></li>
+                    <li><a href="index.php">Log out</a></li>
+                </ul>
+            </div> 
+            <div class="sidebar-bot">
+                <a class="call" href="call.php">New Call</a>
+            </div>
+        </div>
+        <div class="main">
+        	
+        </div>   
 	</body>
 </html>
