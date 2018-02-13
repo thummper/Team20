7409439d8fb4219b8e94fcb1f1864a748d3bc8d3
tomfullylove <tomfullylove@users.noreply.github.com>
2018-02-13 12:45:14 +0000

    query

--- helpdesk/media/php/additional.php
+++ helpdesk/media/php/additional.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML>
 <?php
-<<<<<<< HEAD
+
    if($_POST["save-type"] == "query") {
        include("../../config.php");
        session_start();
@@ -16,7 +16,5 @@
        }else if($_POST["resolved"] == 'Y'){
            
        }
-=======
->>>>>>> 789ddf36ef123d18d820f17dd42a750174c52c5f
    }
 ?>
